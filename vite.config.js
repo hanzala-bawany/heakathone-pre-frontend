5a585ac0fb0a4f3b4795f012969a8747fc9974fe
hanzala bawany <hanzalabawany0@gmail.com>
2025-10-19 07:24:06 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: "./", // 👈 ye line add karo zaroori hai!
+  plugins: [react(), tailwindcss()],
 })
